d6d2b029dfe87a53604a084ea4a9396c972a9535
Ramith <ramith.infoteq@gmail.com>
2019-07-22 14:00:27 +0530

    movie search

--- Assements/frontend/MovieSearch/movie.js
+++ Assements/frontend/MovieSearch/movie.js
@@ -4,11 +4,8 @@
 function searchMovie(){
     var movieName = document.getElementById("search-field").value;
     var searchUrl = "http://www.omdbapi.com/?s="+movieName+"&apikey=a6d715f0";
+    
     fetch(searchUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data) // Prints result from `response.json()` in getRequest
-    })
-    .catch(error => console.error(error))
-    alert("Get tghe movie and search the movies"+movieName);
+    .then(res => res.json())
+    .then(json => console.log(json));
 }
